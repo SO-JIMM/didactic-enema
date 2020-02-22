294ecf61f6afcd3af99a06913882c347ec62d62b
SO-JIMM <jamesdgrace@gmail.com>
2020-02-22 23:31:17 +1100

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+// import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
